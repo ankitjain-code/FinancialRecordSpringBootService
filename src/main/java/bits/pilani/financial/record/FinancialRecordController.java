578b857960d7fbeb0ed41d22ed9b0bf75489474c
ankit <ankitjain19962013@gmail.com>
2021-10-31 10:59:35 +0530

    correcting import errors

--- src/main/java/bits/pilani/financial/record/FinancialRecordController.java
+++ src/main/java/bits/pilani/financial/record/FinancialRecordController.java
@@ -1,54 +1,56 @@
 package bits.pilani.financial.record;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FinancialRecordController {
-
 
 	@Autowired
 	private FinancialRecordRepository repository;
 
 	@GetMapping("/payment/{id}")
 	public FinancialRecord getPaymentRecord(@PathVariable Long id) {
-		
-		Optional<FinancialRecord> paymentRecord = repository.findById(id);	 
+
+		Optional<FinancialRecord> paymentRecord = repository.findById(id);
 
 		return paymentRecord.get();
 	}
-	
-@PostMapping("/payment")
-	public PaymentRecord savePaymentRecord(@RequestBody PaymentRecord paymentRecord) {
-		
-		PaymentRecord exchangeValue = repository.save(paymentRecord);	 
+
+	@PostMapping("/payment")
+	public FinancialRecord savePaymentRecord(@RequestBody FinancialRecord paymentRecord) {
+
+		FinancialRecord exchangeValue = repository.save(paymentRecord);
 
 		return exchangeValue;
 	}
 
-	
 	@GetMapping("/payment/list")
-	public List<PaymentRecord> changePaymentRecord() {
-		
-		List<PaymentRecord> exchangeValue =  new ArrayList<PaymentRecord>();
-		exchangeValue=(List<PaymentRecord>) repository.findAll();	 
+	public List<FinancialRecord> changePaymentRecord() {
+
+		List<FinancialRecord> exchangeValue = new ArrayList<FinancialRecord>();
+		exchangeValue = (List<FinancialRecord>) repository.findAll();
 		return exchangeValue;
 	}
 
 	@GetMapping("/payment/search/amount/{amount}")
-	public PaymentRecord findPaymentRecordByAmount(@PathVariable Float amount) {
-		Optional<PaymentRecord> paymentRecord = repository.findByAmount(amount);
+	public FinancialRecord findPaymentRecordByAmount(@PathVariable Float amount) {
+		Optional<FinancialRecord> paymentRecord = repository.findByAmount(amount);
 		return paymentRecord.get();
 	}
 
 	@GetMapping("/payment/search/currency/{currency}")
-	public List<PaymentRecord> findPaymentRecordByCurr(@PathVariable String currency) {
-		List<PaymentRecord> paymentRecord = repository.findByCurrency(currency);
+	public List<FinancialRecord> findPaymentRecordByCurr(@PathVariable String currency) {
+		List<FinancialRecord> paymentRecord = repository.findByCurrency(currency);
 		return paymentRecord;
 	}
-	
+
 }
--- src/main/java/bits/pilani/financial/record/FinancialRecordRepository.java
+++ src/main/java/bits/pilani/financial/record/FinancialRecordRepository.java
@@ -1,5 +1,6 @@
 package bits.pilani.financial.record;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
@@ -7,7 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 public interface FinancialRecordRepository extends CrudRepository<FinancialRecord, Long> {
 	
 	public Optional<FinancialRecord> findById(Long id);
-	public Optional<PaymentRecord> findByAmount(float amount);
-	public List<PaymentRecord> findByCurrency(String currency);
+	public Optional<FinancialRecord> findByAmount(float amount);
+	public List<FinancialRecord> findByCurrency(String currency);
 	
 }

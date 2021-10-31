271f7b96aeb60ad876e8700f56131c56146ef770
Ankit Jain <ankitjain19962013@gmail.com>
2021-10-31 05:59:46 +0530

    updating master branch (#1)

--- src/main/java/bits/pilani/financial/record/FinancialRecordController.java
+++ src/main/java/bits/pilani/financial/record/FinancialRecordController.java
@@ -1,14 +1,10 @@
 package bits.pilani.financial.record;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
